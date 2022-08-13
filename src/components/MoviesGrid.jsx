c59141287f70856e2f9e64ac18df2b95a8fffa04
Facundo Guardia <contacto@facuguardia.com>
2022-08-12 22:20:41 -0300

    Pegandole a la Api de pelis

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,14 +1,25 @@
 import { MoviesCard } from "./MovieCard";
-import movies from "./movies.json";
 import styles from "./MovieGrid.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export function MoviesGrid() {
-  useEffect (() => {
-    fetch("/discover/movie", {
-      headers: {}
-    })
+  const [movies, setMovies] = useState([]);
+  
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/discover/movie", {
+      headers: {
+        Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs",
+      "Content-Type": "application/json;charset=utf-8",
+      },
   })
+  .then((res) => res.json())
+  .then((data) => {
+    setMovies(data.results);
+  });
+  }, []);
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,16 @@
+import { useParams } from "react-router";
 import movie from "./movie.json";
 import style from "./MovieDetails.module.css";
 
 export function MovieDetails() {
+  const { movieId } = useParams();
+  
+  
   const imgUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
     <div className={style.detailsContainer}>
-      <img className={style.col + " " + style.movieImage} src={imgUrl} alt={movie.title} />
-      <div className={style.col + " " + style.movieDetails}>
+      <img className={`${style.col} ${style.movieImage}`}src={imgUrl} alt={movie.title} />
+      <div className={`${style.col} ${style.movieDetails}`}>
         <p className={style.item}>
           <strong>Title: </strong>
           {movie.title}
