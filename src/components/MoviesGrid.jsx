eaf9add6ebafc12064d94ffcc6d7457c2e2f45e1
Facundo Guardia <contacto@facuguardia.com>
2022-08-14 09:39:11 -0300

    Agregando loading a MoviesGrid.jsx

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -2,15 +2,23 @@ import { MoviesCard } from "./MovieCard";
 import styles from "./MovieGrid.module.css";
 import { useEffect, useState } from "react";
 import { get } from "../utils/httpClient";
+import { Spinner } from "../components/Spinner";
 
 export function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
+
     get("/discover/movie").then((data) => {
       setMovies(data.results);
+      setIsLoading(false);
     });
   }, []);
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
