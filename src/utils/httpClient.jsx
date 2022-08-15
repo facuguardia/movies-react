283ed3c480f56c92cea0771d46682d746d0edb71
Facundo Guardia <contacto@facuguardia.com>
2022-08-15 15:11:47 -0300

    Codigo final

--- src/utils/httpClient.jsx
+++ src/utils/httpClient.jsx
@@ -1,10 +1,10 @@
-const API = "https://api.themoviedb.org/3";
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 export function get(path) {
   return fetch(API + path, {
     headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs",
+      Authorization: "Bearer " + API_TOKEN,
       "Content-Type": "application/json;charset=utf-8",
     },
   }).then((res) => res.json());
